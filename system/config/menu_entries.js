666d99258e47a16f3456a1da1833d705f17ea52c
Darin Rawson <54958995+Darin755@users.noreply.github.com>
2020-10-23 11:07:53 -0600

    Update menu_entries.js

--- system/config/menu_entries.js
+++ system/config/menu_entries.js
@@ -2,7 +2,7 @@
 var entrys = [
 {name: "open", url:"apps/run/index.html"},
 {name: "bing", url:"https://www.bing.com"},
-{name: "caculator", url:"https://www.desmos.com/scientific"},
+{name: "calculator", url:"https://www.desmos.com/scientific"},
 {name: "vm_in_browser", url:"https://copy.sh/v86/"},
 {name: "clock", url:"apps/clock/index.html"},
 {name: "terminal", url:"apps/terminal/index.html"}

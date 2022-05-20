822e470e1765e0de032d5362eb4d23d60508f14c
Darin <54958995+Darin755@users.noreply.github.com>
2022-05-19 20:13:44 -0600

    Switched to DDG

--- system/config/menu_entries.js
+++ system/config/menu_entries.js
@@ -1,7 +1,7 @@
 //entrys for main menu
 var entrys = [
 {name: "open", url:"apps/run/index.html"},
-{name: "bing", url:"https://www.bing.com"},
+{name: "duckduckgo", url:"https://www.duckduckgo.com"},
 {name: "calculator", url:"https://www.desmos.com/scientific"},
 {name: "vm_in_browser", url:"https://copy.sh/v86/"},
 {name: "clock", url:"apps/clock/index.html"},
